8a931a916bddd33832a059290e6cf53836f03e57
rizskie772 <f4dr1234@gmail.com>
2024-08-25 20:16:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path= require('path')
 const QRCode = require('qrcode');
 const Jimp = require('jimp');
 const { pad, toCRC16, dataQris } = require('./lib');
